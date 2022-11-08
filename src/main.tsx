076602ed0b52e6853e7b56b5cba5b29c90b4f1ea
Yevhen <flight.neg@gmail.com>
2022-11-08 12:31:58 +0100

    React 18 main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { CurrencyExchangeApp } from "./CurrencyExchangeApp";
 import "./index.pcss";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <CurrencyExchangeApp />
-  </React.StrictMode>
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
 );
+root.render(<CurrencyExchangeApp />);
